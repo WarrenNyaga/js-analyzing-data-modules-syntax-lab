fb46490b51313e923113138b5171a79a378e5a22
meliodaszeldris <warren.nyaga@gmail.com>
2026-04-15 15:09:19 +0300

    Intitial commit: Add project core files

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function combineUsers(...arrays) {
+  const mergedUsers = arrays.flat();
+  require('datejs');
+  const merge_date = Date.today().toString("M/d/yyyy");
+
+  return {
+    users: mergedUsers,
+    merge_date: merge_date,
+  };
+}
+
+module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
 
 
 
